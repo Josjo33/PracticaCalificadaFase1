610bd7f6d4128b1d9859b7155227bcff01854653
Josjo33 <joshuahugo131@gmail.com>
2025-04-12 12:38:27 -0500

    feat(EmpleadoPorHoras): agregar subclase para empleados por horas

---
+++ Ejercicio1/EmpleadoPorHoras.java
@@ -0,0 +1,25 @@
+package Ejercicio1;
+
+public class EmpleadoPorHoras extends Empleado {
+    private int horasTrabajadas;
+    private double pagoPorHora;
+
+    public EmpleadoPorHoras(String nombre, double salarioBase, int horasTrabajadas, double pagoPorHora) {
+        super(nombre, salarioBase); 
+        this.horasTrabajadas = horasTrabajadas;
+        this.pagoPorHora = pagoPorHora;
+    }
+
+    // Implementación del método abstracto calcularSalarioFinal
+    @Override
+    public double calcularSalarioFinal() {
+        return horasTrabajadas * pagoPorHora;
+    }
+
+    // Implementación del método mostrarInformacion
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Empleado: " + nombre);
+        System.out.println("Salario Final: " + calcularSalarioFinal());
+    }
+}
